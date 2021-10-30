2e5899ea4b51c902d213ab4905c2a5ecdec916af
Jack Henry <jack@jackerickson.net>
2021-10-30 12:42:33 -0500

    work

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Client main");
+use std::net::{IpAddr, TcpStream};
+
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// Client arguments
+struct ClientArguments {
+    #[argh(positional)]
+    host: String,
+
+    #[argh(positional)]
+    port: u8,
+}
+
+fn main() -> std::io::Result<()> {
+    let options: ClientArguments = argh::from_env();
+
+    let ipaddr = match options.host.parse::<IpAddr>() {
+        Ok(ipaddr) => ipaddr,
+        Err(_) => panic!("Invalid ip address provided."),
+    };
+
+    let port = options.port;
+    let url = format!("{}:{}", ipaddr, port);
+    let mut stream = TcpStream::connect(&url)?;
+
+    Ok(())
 }
